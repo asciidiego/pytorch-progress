ad1a7ef97754a6f62a0e833eb13114f99454e07f
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2019-07-21 22:45:45 +0200

    Added functions to show an image

--- 01_deep-learning-with-pytorch/convnet.py
+++ 01_deep-learning-with-pytorch/convnet.py
@@ -106,3 +106,22 @@ testset = torchvision.datasets.CIFAR10(root='./data', train=False,
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                           shuffle=False, num_workers=2)
 
+import matplotlib.pyplot as plt
+import numpy as np
+
+# functions to show an image
+def imshow(img):
+    img = img / 2 + 0.5     # unnormalize
+    npimg = img.numpy()
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+# get some random training images
+dataiter = iter(trainloader)
+images, labels = dataiter.next()
+
+# show images
+imshow(torchvision.utils.make_grid(images))
+# print labels
+print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
