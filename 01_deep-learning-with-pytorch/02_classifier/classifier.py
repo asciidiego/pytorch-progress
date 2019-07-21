6235c548091ae729772fa037a4b88dcdf7288c0c
Diego Rodriguez <diego.vincent.rodriguez@gmail.com>
2019-07-21 23:14:57 +0200

    Built the CPU classifier

--- 01_deep-learning-with-pytorch/02_classifier/classifier.py
+++ 01_deep-learning-with-pytorch/02_classifier/classifier.py
@@ -1,10 +1,10 @@
 import torch
+import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+import torch.optim as optim
 
-import matplotlib.pyplot as plt
-import numpy as np
-
+from model import net
 
 # Loading and normalizing CIFAR10
 transform = transforms.Compose(
@@ -26,20 +26,66 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
            'dog', 'frog', 'horse', 'ship', 'truck')
 
+# Define loss function and optimizer
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
 
-# functions to show an image
-def imshow(img):
-    img = img / 2 + 0.5     # unnormalize
-    npimg = img.numpy()
-    plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
+# Train the network
+for epoch in range(2):  # loop over the dataset multiple times
 
-# get some random training images
-dataiter = iter(trainloader)
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+# Test the network on the test data
+dataiter = iter(testloader)
 images, labels = dataiter.next()
 
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-# show images
+# print images
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+# print predictions
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                              for j in range(4)))
+
+# Show images
 imshow(torchvision.utils.make_grid(images))
+
+
+# Assess
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print('Accuracy of the network on the 10000 test images: %d %%' % (
+    100 * correct / total))
